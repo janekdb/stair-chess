aa05cf65d1f47a2698126e724123d7c9e0d7555f
Janek Bogucki <janekdb@gmail.com>
2012-01-30 20:32:07 +0000

    Add TODO

--- src/chess/model/Misc.scala
+++ src/chess/model/Misc.scala
@@ -9,6 +9,7 @@ object Misc {
     val List(king) = conf.locatePieces(colour, King())
     // TODO: Consolidate this code with the same code as the Castling case by passing a block to
     //   code that finds and iterates over the opponents pieces.
+    // TODO: Confirm this considers non-basic move restriction such as en-passant.
     val opponentPositions = conf.locatePieces(colour.opposite)
     opponentPositions.exists(p =>
       futureMoveExplorer.getBasicPositions(p).contains(king))
