89e5f03f7a8e6198acf2bb2c5175410fc8561d2e
Janek Bogucki <janekdb@gmail.com>
2012-02-02 22:00:47 +0000

    Add StandardMoveExplorerTest; Reduce scope of test imports

--- src/chess/app/ChessApp.scala
+++ src/chess/app/ChessApp.scala
@@ -1,10 +1,8 @@
 package chess.app
 
-import chess.model.{Colours, Move}
+import chess.model.{ Colours, Move }
 import chess.model.BoardModel
-import chess.model.BoardModelTest
-import chess.model.PositionTest
-import chess.player.{Computer, Human}
+import chess.player.{ Computer, Human }
 import chess.ui.UI
 import chess.util.PlayerSelector
 
@@ -12,7 +10,7 @@ object ChessApp {
   def main(args: Array[String]): Unit = {
 
     runTests
-    
+
     val board = new BoardModel
     /* The UI listens for changes and renders them immediately */
     new UI(board).showBoard
@@ -26,9 +24,13 @@ object ChessApp {
       board.move(playerSelector.next.getMove)
     }
   }
-  
+
   def runTests: Unit = {
+    import chess.model.BoardModelTest
+    import chess.model.PositionTest
+    import chess.model.StandardMoveExplorerTest
     BoardModelTest.runTests
     PositionTest.runTests
+    StandardMoveExplorerTest.runTests
   }
 }
