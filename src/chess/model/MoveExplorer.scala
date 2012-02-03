8f725da1a6d7cc4615ec9fbf6423271e09aa710a
Janek Bogucki <janekdb@gmail.com>
2012-02-03 20:38:54 +0000

    Add TODO

--- src/chess/model/MoveExplorer.scala
+++ src/chess/model/MoveExplorer.scala
@@ -5,6 +5,7 @@ package chess.model
  */
 trait MoveExplorer {
 
+  // TODO: Change to Set[Position]
   def getBasicPositions(position: Position): List[Position]
 
   /** @throw IllegalMoveException when the move would violate a condition not considered by {@link getBasicPositions} */
