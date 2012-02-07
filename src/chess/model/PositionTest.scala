1bd44fbcc49a08933cc77497c3598445a65ad31d
Janek Bogucki <janekdb@gmail.com>
2012-02-07 19:01:37 +0000

    Add main

--- src/chess/model/PositionTest.scala
+++ src/chess/model/PositionTest.scala
@@ -4,9 +4,13 @@ import test.Test
 
 object PositionTest extends Test {
 
+  // TODO: Find out how to only define this in the superclass  
+  def main(args: Array[String]): Unit = {
+    runTests
+  }
+
   def runTests(): Unit = {
     val i = Position.getInterveningPositions(new Position(1, 2), new Position(4, 2))
-    println(List.range(2, 4))
     if (i != List(new Position(2, 2), new Position(3, 2))) {
       println(i)
       fail
