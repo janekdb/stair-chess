a6e1b50d064d34d98f59595262f85d8dc6213cd6
Janek Bogucki <janekdb@gmail.com>
2012-02-07 19:05:30 +0000

    Add failed assertion message

--- src/chess/model/PositionTest.scala
+++ src/chess/model/PositionTest.scala
@@ -13,7 +13,7 @@ object PositionTest extends Test {
     val i = Position.getInterveningPositions(new Position(1, 2), new Position(4, 2))
     if (i != List(new Position(2, 2), new Position(3, 2))) {
       println(i)
-      fail
+      fail("The list of intervening positions was unexpected: " + i.toString)
     }
   }
 }
