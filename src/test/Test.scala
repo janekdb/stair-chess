66d508daca6fbc335bb1308128ece2af2b1a6e3c
Janek Bogucki <janekdb@gmail.com>
2012-03-05 19:16:26 +0000

    Add asssertEquals(,)

--- src/test/Test.scala
+++ src/test/Test.scala
@@ -7,16 +7,20 @@ trait Test {
   def fail: Unit = throw new FailureException("fail")
 
   def fail(message: String): Unit = throw new FailureException(message)
-  
+
   def fail(e: Exception): Unit = throw new FailureException(e)
 
   def unexpected(e: Exception): Unit = throw new FailureException(e, "Unexpected exception:" + e.getClass.getName)
-  
+
+  def assertEquals(expected: Any, actual: Any) = {
+    assert(expected == actual, "Expected: " + expected + ", actual: " + actual)
+  }
+
   class FailureException(e: Exception, message: String) extends RuntimeException(message, e) {
-    
+
     // TODO: Add the name of the unexpected exception to the message.
     def this(e: Exception) = this(e, "Unexpected exception" + e.getClass())
-    
+
     def this(message: String) = this(null, message)
 
   }
