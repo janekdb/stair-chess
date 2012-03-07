f393f05cafa3857c3a4605d3c659dffa2518dbd5
Janek Bogucki <janekdb@gmail.com>
2012-03-07 19:33:54 +0000

    Clarify TODO

--- src/chess/model/GridConfigurationTest.scala
+++ src/chess/model/GridConfigurationTest.scala
@@ -9,7 +9,6 @@ object GridConfigurationTest extends Test {
   // TODO: Define these test helping implicits in a common location
   implicit def piece2List(t: Piece) = List(t)
   implicit def string2Position(s: String) = new Position(s)
-  // TODO: Replace new MovePiece(x) with x
   //  implicit def string2MovePiece(s: String) = new MovePiece(s)
 
   // TODO: Find out how to only define this in the superclass  
