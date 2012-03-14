b1b70b9e93d67ee834a3ead46328e5de53ada4ed
Janek Bogucki <janekdb@gmail.com>
2012-03-14 19:13:57 +0000

    Overload throwRuntimeEx with message argument

--- src/chess/util/TODO.scala
+++ src/chess/util/TODO.scala
@@ -2,7 +2,9 @@ package chess.util
 
 object TODO {
 
-  def log(s: String) : Unit = { println(s) }
-  
-  def throwRuntimeEx: Nothing = throw new RuntimeException("TODO") 
+  def log(s: String): Unit = { println(s) }
+
+  def throwRuntimeEx: Nothing = throw new RuntimeException("TODO")
+
+  def throwRuntimeEx(message: String): Nothing = throw new RuntimeException("TODO: " + message)
 }
