24fae1f87fa144c6b30af2dab85b9a73e6ab810d
Janek Bogucki <janekdb@gmail.com>
2012-03-28 20:12:23 +0100

    Add SimpleMove

--- src/chess/model/Move.scala
+++ src/chess/model/Move.scala
@@ -6,13 +6,19 @@ import ex.IllegalPromotionException
 
 abstract class Move
 
+/* A simple move has start and end positions */
+abstract class SimpleMove extends Move {
+  def start: Position
+  def end: Position
+}
+
 /* Assume the consumer of MovePiece has access to the board configuration. */
-case class MovePiece(start: Position, end: Position) extends Move {
+case class MovePiece(start: Position, end: Position) extends SimpleMove {
   def this(move: String) = this(new Position(move.substring(0,2)), new Position(move.substring(2,4)))
 }
 case class Resign(colour: Colour) extends Move
 case class Castle(colour: Colour, castlingType: CastlingType) extends Move
-case class Promote(start: Position, end: Position, piece: Piece) extends Move {
+case class Promote(start: Position, end: Position, piece: Piece) extends SimpleMove {
   def this(move: String, piece: Piece) = {
     this(new Position(move.substring(0,2)), new Position(move.substring(2,4)), piece)
     if(List(King(), Pawn()) contains piece){
@@ -20,6 +26,6 @@ case class Promote(start: Position, end: Position, piece: Piece) extends Move {
     }
   }
 }
-case class EnPassant(start: Position, end: Position) extends Move {
+case class EnPassant(start: Position, end: Position) extends SimpleMove {
   val taken = new Position(end.getCol, start.getRow)
 }
