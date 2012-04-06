b812610bb1c67adeec10d870429577eee2831a07
Janek Bogucki <janekdb@gmail.com>
2012-04-06 18:20:07 +0100

    Remove TODO

--- src/chess/model/GridConfiguration.scala
+++ src/chess/model/GridConfiguration.scala
@@ -36,7 +36,6 @@ class GridConfiguration extends Configuration {
 
   private var lastMove: Option[(Piece, Position, Position)] = None
 
-  // TODO: Drop getLastMove in favour of compiler provided getter without a setter
   def getLastMove: Option[(Piece, Position, Position)] = lastMove
 
   /** Replace the piece with a the same colour carrying over the previous position */
