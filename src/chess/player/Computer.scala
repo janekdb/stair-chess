56d1f52ab397326d6c4a0c26acff11338f7a2ce3
Janek Bogucki <janekdb@gmail.com>
2012-04-26 20:18:40 +0100

    Replace test game with Scholars Materc/chess/player/Human.scala

--- src/chess/player/Computer.scala
+++ src/chess/player/Computer.scala
@@ -7,18 +7,11 @@ class Computer(val board: BoardModel, val colour: Colour) extends Player {
   implicit def stringToMovePiece(s: String) = new MovePiece(s)
 
   var moves: List[Move] = List(
-    "a2a3",
-    "b2b4",
-    "c2c4",
-    "h2h3",
-    /* Knight */
-    "b1c3",
-    /* Knight */
-    "g1f3",
-    /* Knight takes pawn */
-    "c3d5",
-    "c1b2",
-    "b4b5")
+      "e2e4",
+      "d1h5",
+      "f1c4",
+      "h5f7"
+    )
 
   def getMove: Move = {
     val m :: ms = moves
