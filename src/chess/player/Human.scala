db2b422f189d6927530abd0e337c68befc1fc655
Janek Bogucki <janekdb@gmail.com>
2012-04-26 20:19:03 +0100

    Replace test game with Scholars Mate

--- src/chess/player/Human.scala
+++ src/chess/player/Human.scala
@@ -8,20 +8,11 @@ class Human(val board: BoardModel, val colour: Colour) extends Player {
   implicit def stringToMovePiece(s: String) = new MovePiece(s)
 
   var moves: List[Move] = List(
-    "a7a6",
-    "d7d5",
-    "e7e5",
-    "f7f6",
-    /* Queen */
-    "d8d7",
-    /* Rook */
-    "a8a7",
-    /* Knight */
-    "g8h6",
-    /* Move Bishop to allow short castle */
-    "f8e7",
-
-    Castle(colour, Short))
+      "e7e5",
+      "b8c6",
+      "g8f6"
+    )
+    
   def getMove: Move = {
     val m :: ms = moves
     moves = ms
