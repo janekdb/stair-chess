1f7cbec32c6e810e5c07fe22a111cedffc5693ed
Janek Bogucki <janekdb@gmail.com>
2012-05-04 08:40:52 +0100

    Replace references to label with piece

--- src/chess/ui/Board.java
+++ src/chess/ui/Board.java
@@ -1,16 +1,17 @@
 package chess.ui;
 
-
+// TODO: Consider removing this interface and using just BoardChangedSubscriber
 public interface Board {
 
 	/** 1-based */
-	void clearLabel(final int col, final int row);
+	void clearSquare(final int col, final int row);
 
 	/** 1-based */
-	void setLabel(final int col, final int row, final String label);
+	void setPiece(final int col, final int row, final String piece);
 
 	// TODO: Remove this method once the Board has access to a Configuration
 	/** 1-based */
-	String getLabel(final int col, final int row);
+	String getPiece(final int col, final int row);
 
+	void showWon(final String colour, final String wonMode);
 }
