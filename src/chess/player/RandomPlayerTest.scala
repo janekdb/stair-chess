bbba7d8806eeddedf66bf423c887c4dbaf242e73
Janek Bogucki <janekdb@gmail.com>
2012-05-10 18:15:23 +0100

    Add test for RandomPlayer

---
+++ src/chess/player/RandomPlayerTest.scala
@@ -0,0 +1,46 @@
+package chess.player
+
+import test.{ Main, Test, TestUtils }
+import chess.model._
+import chess.model.Colours._
+
+object RandomPlayerTest extends Test with TestUtils with Main {
+
+  def runTests {
+    canMove
+    isRandom
+    selectsOnlyMove
+  }
+
+  private def canMove {
+    val rp = newRandomPlayer
+    assertNotNull(rp.getMove, "A move should be available")
+  }
+
+  private def isRandom {
+    val conf: Configuration = new GridConfiguration
+    val rp = newRandomPlayer(conf)
+    val m1 = rp.getMove
+    conf.applyMove(m1)
+    val m2 = rp.getMove
+    assertNotEquals(m1, m2, "The second move should be different to the first")
+  }
+
+  private def selectsOnlyMove {
+    val conf: Configuration = new GridConfiguration
+    conf.add("a6", White, Pawn());
+    conf.add("a7", White, Rook());
+    conf.add("b8", Black, Rook());
+    val rp = newRandomPlayer(conf)
+    val m = rp.getMove
+    assertEquals(MovePiece("a7", "a8"), m, "The only possible move should have been selected")
+  }
+
+  private def newRandomPlayer(conf: Configuration): Player = {
+    conf.add("a1", White, Rook())
+    val explorer: MoveExplorer = new StandardMoveExplorer(conf)
+    new RandomPlayer(White, conf, explorer)
+  }
+
+  private def newRandomPlayer: Player = newRandomPlayer(new GridConfiguration)
+}
