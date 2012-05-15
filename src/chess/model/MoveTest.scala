c7e9da5bf99ca9d37e5e4f2c6e08c29c5c011212
Janek Bogucki <janekdb@gmail.com>
2012-05-15 20:14:45 +0100

    Test for rejection of move with a start position which is the same as the end position

--- src/chess/model/MoveTest.scala
+++ src/chess/model/MoveTest.scala
@@ -1,15 +1,23 @@
 package chess.model
 
-import test.{Main, Test, TestUtils}
+import test.{ Main, Test, TestUtils }
 
 object MoveTest extends Test with TestUtils with Main {
 
   // TODO: Remove parens on all runTests
   def runTests() {
+    rejectWhenStartEqualsEnd
     enPassant
   }
 
-  private def enPassant = {
+  private def rejectWhenStartEqualsEnd {
+    assertExceptionThrown("A move with the start equal to the end position should be rejected", classOf[IllegalArgumentException]) {
+      val p = new Position("e7")
+      MovePiece(p, p)
+    }
+  }
+
+  private def enPassant {
     val e = EnPassant("e5", "d6")
     assertEquals(new Position("d5"), e.taken)
   }
