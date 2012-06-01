9e2581a345d13107a7dbe9b421e8ad68e647f003
Janek Bogucki <janekdb@gmail.com>
2012-06-01 18:47:07 +0100

    Add legalMoves method

--- src/chess/model/MoveExplorer.scala
+++ src/chess/model/MoveExplorer.scala
@@ -5,6 +5,7 @@ package chess.model
  */
 trait MoveExplorer {
 
+  // TODO: Consider make private if legalMoves is sufficient for clients
   def getBasicPositions(position: Position): Set[Position]
 
   /** @throw IllegalMoveException when the move would violate a condition not considered by {@link getBasicPositions} */
@@ -13,4 +14,6 @@ trait MoveExplorer {
   /** @return true if the king of the selected colour is in check */
   def kingInCheck(colour: Colour): Boolean
 
+  /** @return All legal moves. */
+  def legalMoves(colour: Colour): List[Move]
 }
