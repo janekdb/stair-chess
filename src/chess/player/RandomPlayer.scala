1997e6909ccbccaebe2161c34b7bb0aeed208281
Janek Bogucki <janekdb@gmail.com>
2012-06-01 18:56:06 +0100

    Replace move generation code with use of MoveExplorer

--- src/chess/player/RandomPlayer.scala
+++ src/chess/player/RandomPlayer.scala
@@ -1,17 +1,8 @@
 package chess.player
 
-import chess.model.{ Castle, EnPassant, Move, MovePiece, MovePieceCapturing, Promote, PromoteCapturing }
+import chess.model.Move
 import chess.model.{ Configuration, MoveExplorer }
-import chess.model.ex.IllegalMoveException
 import chess.model.Colour
-import chess.model.Position
-import chess.model.Pawn
-import chess.model.Constants
-import chess.model.{Knight, Queen}
-import chess.model.{ Long, Short }
-
-// TODO: Move move generation to StandardMoveExplorer
-// TODO: Rework checkmate escape code to use list of valid moves
 
 /**
  * A player that selects moves at random.
@@ -19,50 +10,9 @@ import chess.model.{ Long, Short }
 class RandomPlayer(val colour: Colour, val conf: Configuration, val explorer: MoveExplorer) extends Player {
 
   def getMove: Move = {
-    val startPositions = conf.locatePieces(colour)
-    def isHomeRow(row: Int): Boolean = List(Constants.WHITE_HOME_ROW, Constants.BLACK_HOME_ROW) contains row
-    def isDiagonal(a: Position, b: Position): Boolean = a.col != b.col
-    var moves = List[Move]()
-    val ps = List(Knight(), Queen())
-    for (s <- startPositions) {
-      var endPositions = explorer.getBasicPositions(s)
-      val (_, piece, _) = conf.getExistingPiece(s)
-      // TODO: Switch to functional approach with yield
-      endPositions.foreach { end =>
-        val endOccupied = conf.getPiece(end).isDefined
-        val ms = piece match {
-          case Pawn() if isHomeRow(end.getRow) =>
-            if (endOccupied) ps.map {PromoteCapturing(s, end, _)} else ps.map {Promote(s, end, _)}
-          case Pawn() if (!endOccupied && isDiagonal(s, end)) => List(EnPassant(s, end))
-          case default => if (endOccupied) List(MovePieceCapturing(s, end)) else List(MovePiece(s, end))
-        }
-        moves = ms ::: moves
-      }
-    }
-
-    /*
-     * Only add castling if the king and rooks are at the correct positions because
-     * rejectIllegalMove does not explicitly check the pieces are present.
-     */
-    for (castlingType <- List(Long, Short)) {
-      val ((king, _), (rook, _)) = castlingType.getPositions(colour.homeRow)
-      if (List(king, rook).forall(conf.exists(_, colour))) {
-        moves = Castle(colour, castlingType) :: moves
-      }
-    }
-    moves = moves filter { moveAcceptable }
+    val moves = explorer.legalMoves(colour)
     if (moves.isEmpty) throw new RuntimeException("No move found")
     moves(new util.Random().nextInt(moves.size))
-  }
-
-  protected def moveAcceptable(move: Move): Boolean = {
-    try {
-      // TODO: Convert rejectIllegalMove to a query method
-      explorer.rejectIllegalMove(move)
-      true
-    } catch {
-      case e: IllegalMoveException=> false
-    }
   }
 
   // TODO: Research Fisher-Yates shuffling algorithm.
