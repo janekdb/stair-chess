8e070f02ab43914f774521feb6ad18536568dcca
Janek Bogucki <janekdb@gmail.com>
2012-06-01 23:32:08 +0100

    Consolidate start and end position extraction with objects

--- src/chess/model/Move.scala
+++ src/chess/model/Move.scala
@@ -4,10 +4,17 @@ import ex.IllegalPromotionException
 
 
 //class StringOps(val s: String) {
-//  def start = new Position(s.substring(0, 2))
-//  def end = new Position(s.substring(2, 4))
+//  def start = s.substring(0, 2)
+//  def end = s.substring(2, 4)
 //}
 //implicit def stringOps(s: String) = new StringOps(s)
+
+object start {
+  def apply(move: String): Position = new Position(move.substring(0, 2))
+}
+object end {
+	def apply(move: String): Position = new Position(move.substring(2, 4))
+}
 
 /** Verbs */
 
@@ -22,40 +29,35 @@ abstract class SimpleMove extends Move {
 /* Assume the consumer of MovePiece has access to the board configuration. */
 case class MovePiece(val start: Position, val end: Position) extends SimpleMove {
   require(start != end, "Start must be different to end: " + start)
-  // TODO: Add methods to move.{start, end} with an implicit operator
-  // TODO: Determine if an extractor could use use to parse "a1a2" into an instance of MovePiece
-  def this(move: String) = this(new Position(move.substring(0, 2)), new Position(move.substring(2, 4)))
+  def this(move: String) = this(start(move), end(move))
 }
 case class MovePieceCapturing(val start: Position, val end: Position) extends SimpleMove {
   require(start != end, "Start must be different to end: " + start)
-  // TODO: Add methods to move.{start, end} with an implicit operator
-  // TODO: Determine if an extractor could use use to parse "a1a2" into an instance of MovePiece
-  def this(move: String) = this(new Position(move.substring(0, 2)), new Position(move.substring(2, 4)))
+  def this(move: String) = this(start(move), end(move))
 }
 case class Resign(colour: Colour) extends Move
 case class Castle(colour: Colour, castlingType: CastlingType) extends Move
 case class Promote(val start: Position, val end: Position, val piece: Piece) extends SimpleMove {
+  require(start != end, "Start must be different to end: " + start)
   // TODO: Remove end because for promotion without capturing the start determines the end
   def this(move: String, piece: Piece) = {
-    // TODO: Add methods to move.{start, end} with an implicit operator
-    // TODO: Determine if an extractor could use use to parse "a1a2" into an instance of MovePiece
-    this(new Position(move.substring(0, 2)), new Position(move.substring(2, 4)), piece)
+    this(start(move), end(move), piece)
     if (List(King(), Pawn()) contains piece) {
       throw new IllegalPromotionException(piece)
     }
   }
 }
 case class PromoteCapturing(val start: Position, val end: Position, val piece: Piece) extends SimpleMove {
+  require(start != end, "Start must be different to end: " + start)
   // TODO: Reduce duplication with Promote
   def this(move: String, piece: Piece) = {
-    // TODO: Add methods to move.{start, end} with an implicit operator
-    // TODO: Determine if an extractor could use use to parse "a1a2" into an instance of MovePiece
-    this(new Position(move.substring(0, 2)), new Position(move.substring(2, 4)), piece)
+    this(start(move), end(move), piece)
     if (List(King(), Pawn()) contains piece) {
       throw new IllegalPromotionException(piece)
     }
   }
 }
 case class EnPassant(val start: Position, val end: Position) extends SimpleMove {
+  require(start != end, "Start must be different to end: " + start)
   val captured = new Position(end.getCol, start.getRow)
 }
