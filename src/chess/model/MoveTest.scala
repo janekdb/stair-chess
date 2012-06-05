742137fb06c8870b0fdfcee1310008ad3c2c17d8
Janek Bogucki <janekdb@gmail.com>
2012-06-05 18:55:03 +0100

    Add test for end position for Promote

--- src/chess/model/MoveTest.scala
+++ src/chess/model/MoveTest.scala
@@ -16,8 +16,8 @@ object MoveTest extends Test with TestUtils with Main {
       MovePiece(p, p)
     }
     assertExceptionThrown("A move with the start equal to the end position should be rejected", classOf[IllegalArgumentException]) {
-    	val p = new Position("e7")
-    	MovePieceCapturing(p, p)
+      val p = new Position("e7")
+      MovePieceCapturing(p, p)
     }
   }
 
@@ -26,4 +26,10 @@ object MoveTest extends Test with TestUtils with Main {
     assertEquals(new Position("d5"), e.captured)
   }
 
+  private def promote {
+    val wp = Promote("a7", Queen())
+    assertEquals(wp.end, new Position("a8"), "The end position of a Promote was correct for White")
+    val bp = Promote("a2", Queen())
+    assertEquals(bp.end, new Position("a1"), "The end position of a Promote was correct for Black")
+  }
 }
