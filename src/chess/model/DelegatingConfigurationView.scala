9f231704507c7996f9edf2c44014cb80cdbde897
Janek Bogucki <janekdb@gmail.com>
2012-06-12 19:07:35 +0100

    Add class to provide read only access to a Configuration

---
+++ src/chess/model/DelegatingConfigurationView.scala
@@ -0,0 +1,22 @@
+package chess.model
+
+import scala.collection.immutable.List
+
+/**
+ * Provide an implementation of ConfigurationView by delegating to a Configuration
+ */
+class DelegatingConfigurationView(val configuration: Configuration) extends ConfigurationView {
+
+  def getLastMove(): Option[(Piece, Position, Position)] = configuration.getLastMove
+
+  def getRows(): List[List[(Colour, Piece)]] = configuration.getRows
+
+  def getExistingPiece(position: Position): (Colour, Piece, Option[Position]) = configuration.getExistingPiece(position)
+
+  def getPiece(position: Position): Option[(Colour, Piece, Option[Position])] = getPiece(position)
+
+  def locatePieces(colour: Colour, piece: Piece): List[Position] = configuration.locatePieces(colour, piece)
+
+  def locatePieces(colour: Colour): List[Position] = configuration.locatePieces(colour)
+
+}
