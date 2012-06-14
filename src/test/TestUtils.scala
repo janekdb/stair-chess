01b50f9607f6925708d3d32d781030d481199e9c
Janek Bogucki <janekdb@gmail.com>
2012-06-14 20:46:45 +0100

    Support Option[Move]

--- src/test/TestUtils.scala
+++ src/test/TestUtils.scala
@@ -1,12 +1,14 @@
 package test
 
-import chess.model.{Piece, Position, MovePiece}
+import chess.model.{Move, Piece, Position, MovePiece}
 
 /** Chess specifc helpers to simplify tests. */
 trait TestUtils {
 
   implicit def piece2List(t: Piece) = List(t)
+  implicit def string2MovePieceOption(s: String) = Some(new MovePiece(s))
   implicit def string2MovePiece(s: String) = new MovePiece(s)
+  implicit def moveMoveOption(m: Move) = Some(m)
   implicit def string2Position(s: String) = new Position(s)
 
 }
