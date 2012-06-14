74d6d4b0bccec4e1bab49bff0c60775a3bd80f0c
Janek Bogucki <janekdb@gmail.com>
2012-06-14 08:59:28 +0100

    Categorise capturing moves with Capturing trait

--- src/chess/model/Move.scala
+++ src/chess/model/Move.scala
@@ -24,12 +24,14 @@ abstract class SimpleMove extends Move {
   def end: Position
 }
 
+trait Capturing
+
 /* Assume the consumer of MovePiece has access to the board configuration. */
 case class MovePiece(val start: Position, val end: Position) extends SimpleMove {
   require(start != end, "Start must be different to end: " + start)
   def this(move: String) = this(start(move), end(move))
 }
-case class MovePieceCapturing(val start: Position, val end: Position) extends SimpleMove {
+case class MovePieceCapturing(val start: Position, val end: Position) extends SimpleMove with Capturing {
   require(start != end, "Start must be different to end: " + start)
   def this(move: String) = this(start(move), end(move))
 }
@@ -45,14 +47,14 @@ case class Promote(val start: Position, val piece: Piece) extends SimpleMove {
     rejectInvalidPromotionPiece(piece)
   }
 }
-case class PromoteCapturing(val start: Position, val end: Position, val piece: Piece) extends SimpleMove {
+case class PromoteCapturing(val start: Position, val end: Position, val piece: Piece) extends SimpleMove with Capturing {
   require(start != end, "Start must be different to end: " + start)
   def this(move: String, piece: Piece) = {
     this(start(move), end(move), piece)
     rejectInvalidPromotionPiece(piece)
   }
 }
-case class EnPassant(val start: Position, val end: Position) extends SimpleMove {
+case class EnPassant(val start: Position, val end: Position) extends SimpleMove with Capturing {
   require(start != end, "Start must be different to end: " + start)
   val captured = new Position(end.getCol, start.getRow)
 }
