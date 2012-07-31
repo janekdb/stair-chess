b661a699201b3775fb65606b84edb39065af5719
Janek Bogucki <janekdb@gmail.com>
2012-07-31 20:29:17 +0100

    Order wins and draws

--- src/chess/app/ScoreCard.scala
+++ src/chess/app/ScoreCard.scala
@@ -1,33 +1,68 @@
 package chess.app
 
 import scala.collection.mutable.HashMap
+
 import chess.player.Player
 
-// TODO: Add ScoreCardTest
 class ScoreCard {
-  val scores = new HashMap[String, Int]() { override def default(k: String) = 0 }
+  val wins = new HashMap[String, Int]() { override def default(k: String) = 0 }
+  val draws = new HashMap[String, Int]() { override def default(k: String) = 0 }
 
   def addWin(winner: Player, loser: Player) {
     println("Adding win for " + winner)
-    val record = winner.getName + " beat " + loser.getName
 
-    scores(record) = scores(record) + 1
+    wins(winner.getName) = wins(winner.getName) + 1
+    wins(loser.getName) = wins(loser.getName) + 0
+
+    draws(winner.getName) = draws(winner.getName) + 0
+    draws(loser.getName) = draws(loser.getName) + 0
   }
 
-  def addDraw(player1: Player, player2: Player){
-    // TODO: Record draw
-  }
-  
-  def addAbandon(player1: Player, player2: Player){
-    // TODO: Record abandon
-  }
-  
-  override def toString = scores.toString
+  def addDraw(player1: Player, player2: Player) {
+    draws(player1.getName) = draws(player1.getName) + 1
+    draws(player2.getName) = draws(player2.getName) + 1
 
+    wins(player1.getName) = wins(player1.getName) + 0
+    wins(player2.getName) = wins(player2.getName) + 0
+  }
+
+  def getWins: List[(String, Int)] = {
+    /* Sort the greatest number of wins first */
+    val ordering = new Object with Ordering[(String, Int)] {
+      def compare(x: (String, Int), y: (String, Int)) = {
+        val (p1, s1) = x
+        val (p2, s2) = y
+        val sc = -(s1 compare s2)
+        /* For tied scores order by name */
+        if (sc != 0) sc else p1 compare p2
+      }
+    }
+    wins.toList.sorted(ordering)
+  }
+
+  def getDraws: List[(String, Int)] = {
+    /* Sort by name because a high draw count is not neccesarily better or worse than a low number */
+    val ordering = new Object with Ordering[(String, Int)] {
+      def compare(x: (String, Int), y: (String, Int)) = {
+        val (p1, _) = x
+        val (p2, _) = y
+        p1 compare p2
+      }
+    }
+    draws.toList.sorted(ordering)
+  }
+
+  def getAbandons: List[(String, Int)] = {
+    List()
+  }
+
+  override def toString = wins.toString
+
+  // TODO: Remove this method
   // TODO: Display victory scores first ordered by highest number of wins
   // TODO: Move ScoreCard into suitable package
   def displayScores {
-    scores.foreach {
+    wins.foreach {
       case (k, v) => println(k + ": " + v)
     }
   }
---
+++ src/chess/app/ScoreCardTest.scala
@@ -0,0 +1,59 @@
+package chess.app
+
+import chess.player.Player
+import chess.model.Configuration
+
+import test.{ Main, Test, TestUtils }
+
+object ScoreCardTest extends Test with TestUtils with Main {
+
+  def runTests {
+    emptyScoreCardHasEmptyScores
+    winsOrderedByMostFirst
+    drawsOrderedByName
+  }
+
+  private class PlayerBase(name: String) extends Player {
+    def getName = name
+    def getMove(configuration: Configuration) = None
+  }
+
+  private val PLAYER_1 = new PlayerBase("player-1")
+  private val PLAYER_2 = new PlayerBase("player-2")
+  private val PLAYER_3 = new PlayerBase("player-3")
+  private val PLAYER_4 = new PlayerBase("player-4")
+  private val PLAYER_5 = new PlayerBase("player-5")
+
+  private def emptyScoreCardHasEmptyScores {
+    val sc = new ScoreCard
+    assertEquals(List(), sc.getWins)
+    assertEquals(List(), sc.getDraws)
+    assertEquals(List(), sc.getAbandons)
+  }
+
+  private def winsOrderedByMostFirst {
+    val sc = new ScoreCard
+    sc.addWin(PLAYER_1, PLAYER_2)
+    sc.addWin(PLAYER_1, PLAYER_4)
+    sc.addWin(PLAYER_4, PLAYER_1)
+    sc.addWin(PLAYER_2, PLAYER_3)
+    /* No win for player 5 */
+    sc.addDraw(PLAYER_1, PLAYER_5)
+    val wins = sc.getWins
+    assertEquals(List((PLAYER_1.getName, 2), (PLAYER_2.getName, 1), (PLAYER_4.getName, 1), (PLAYER_3.getName, 0), (PLAYER_5.getName, 0)), wins)
+  }
+
+  private def drawsOrderedByName {
+    val sc = new ScoreCard
+    sc.addDraw(PLAYER_1, PLAYER_2)
+    sc.addDraw(PLAYER_1, PLAYER_4)
+    sc.addDraw(PLAYER_2, PLAYER_3)
+    /* No draw for player 5 */
+    sc.addWin(PLAYER_1, PLAYER_5)
+    val draws = sc.getDraws
+    assertEquals(List((PLAYER_1.getName, 2), (PLAYER_2.getName, 2), (PLAYER_3.getName, 1), (PLAYER_4.getName, 1), (PLAYER_5.getName, 0)), draws)
+
+    fail("Initialise ScoreCard with list of all player")
+  }
+
+}
