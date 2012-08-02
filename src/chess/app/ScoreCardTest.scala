ef31259dec47939a53ac61eda6b8e93735676798
Janek Bogucki <janekdb@gmail.com>
2012-08-02 18:46:05 +0100

    Supply all competitors in constructor

--- src/chess/app/ScoreCardTest.scala
+++ src/chess/app/ScoreCardTest.scala
@@ -7,6 +7,8 @@ import test.{ Main, Test, TestUtils }
 
 object ScoreCardTest extends Test with TestUtils with Main {
 
+  private implicit def player2String(player: Player): String = player.getName
+
   def runTests {
     emptyScoreCardHasEmptyScores
     winsOrderedByMostFirst
@@ -25,14 +27,13 @@ object ScoreCardTest extends Test with TestUtils with Main {
   private val PLAYER_5 = new PlayerBase("player-5")
 
   private def emptyScoreCardHasEmptyScores {
-    val sc = new ScoreCard
+    val sc = new ScoreCard(Set())
     assertEquals(List(), sc.getWins)
     assertEquals(List(), sc.getDraws)
-    assertEquals(List(), sc.getAbandons)
   }
 
   private def winsOrderedByMostFirst {
-    val sc = new ScoreCard
+    val sc = new ScoreCard(Set(PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4, PLAYER_5))
     sc.addWin(PLAYER_1, PLAYER_2)
     sc.addWin(PLAYER_1, PLAYER_4)
     sc.addWin(PLAYER_4, PLAYER_1)
@@ -44,7 +45,7 @@ object ScoreCardTest extends Test with TestUtils with Main {
   }
 
   private def drawsOrderedByName {
-    val sc = new ScoreCard
+    val sc = new ScoreCard(Set(PLAYER_1, PLAYER_2, PLAYER_3, PLAYER_4, PLAYER_5))
     sc.addDraw(PLAYER_1, PLAYER_2)
     sc.addDraw(PLAYER_1, PLAYER_4)
     sc.addDraw(PLAYER_2, PLAYER_3)
@@ -52,8 +53,6 @@ object ScoreCardTest extends Test with TestUtils with Main {
     sc.addWin(PLAYER_1, PLAYER_5)
     val draws = sc.getDraws
     assertEquals(List((PLAYER_1.getName, 2), (PLAYER_2.getName, 2), (PLAYER_3.getName, 1), (PLAYER_4.getName, 1), (PLAYER_5.getName, 0)), draws)
-
-    fail("Initialise ScoreCard with list of all player")
   }
 
 }
