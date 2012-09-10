2dfcf7c80f3ed7dbe6385eda115d808a13ce7312
Janek Bogucki <janekdb@gmail.com>
2012-09-10 20:02:23 +0100

    Add onPiecePlaced to allow PiecePlaced to be moved out of BoardChanged hierarchy

--- src/chess/model/BoardChangedSubscriber.scala
+++ src/chess/model/BoardChangedSubscriber.scala
@@ -6,4 +6,10 @@ trait BoardChangedSubscriber {
    * @param events The parts comprising a single move.
    */
   def onBoardChanged(events: List[BoardChanged])
+
+  /**
+   * @param event Details of the piece that was placed during the
+   * board setup.
+   */
+  def onPiecePlaced(event: PiecePlaced)
 }
