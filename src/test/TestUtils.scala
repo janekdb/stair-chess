4a88399fd1c0b0db05304d176090a6703940c771
Janek Bogucki <janekdb@gmail.com>
2012-09-17 19:23:08 +0100

    Pullup addXXXKing

--- src/test/TestUtils.scala
+++ src/test/TestUtils.scala
@@ -1,8 +1,11 @@
 package test
 
-import chess.model.{Move, Piece, Position, MovePiece}
+import chess.model.{ Move, Piece, Position, MovePiece }
+import chess.model.Configuration
+import chess.model.Colours.{Black,White}
+import chess.model.King
 
-/** Chess specifc helpers to simplify tests. */
+/** Chess specific helpers to simplify tests. */
 trait TestUtils {
 
   implicit def piece2List(t: Piece) = List(t)
@@ -10,5 +13,15 @@ trait TestUtils {
   implicit def string2MovePiece(s: String) = new MovePiece(s)
   implicit def moveMoveOption(m: Move) = Some(m)
   implicit def string2Position(s: String) = new Position(s)
+
+  protected def addWhiteKing(conf: Configuration) {
+    /* The King is required to allow the kingInCheck method to complete. */
+    conf.add("e1", White, King())
+  }
+
+  protected def addBlackKing(conf: Configuration) {
+    /* The King is required to allow the kingInCheck method to complete. */
+    conf.add("e8", Black, King())
+  }
 
 }
