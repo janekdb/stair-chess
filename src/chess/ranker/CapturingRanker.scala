fe26dd7034e7e6674c0a04a2dfa5657653f1f786
Janek Bogucki <janekdb@gmail.com>
2012-11-15 21:16:38 +0000

    Rank capturing moves above others

---
+++ src/chess/ranker/CapturingRanker.scala
@@ -0,0 +1,18 @@
+package chess.ranker
+import chess.model.Colour
+import chess.model.MoveExplorer
+import chess.model.ConfigurationView
+import chess.model.Move
+import chess.model.Capturing
+
+/**
+ * An implementation of MoveRanker that ranks capturing moves higher than other moves.
+ */
+class CapturingRanker(val explorerFactory: ConfigurationView => MoveExplorer, colour: Colour) extends MoveRanker {
+
+  private def rank(move: Move) = move match { case _: Capturing => 0 case default => 1 }
+
+  def rankMoves(moves: List[Move], conf: ConfigurationView): List[List[Move]] =
+    moves.groupBy(rank).toList.sortBy(_._1).map(_._2)
+
+}
