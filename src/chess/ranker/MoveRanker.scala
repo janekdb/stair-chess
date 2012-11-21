359b81d42162145ac2509eeaf8ddcef485c77f19
Janek Bogucki <janekdb@gmail.com>
2012-11-21 19:31:25 +0000

    Add constants for use when performing a binary ranking

--- src/chess/ranker/MoveRanker.scala
+++ src/chess/ranker/MoveRanker.scala
@@ -3,6 +3,11 @@ import chess.model.Move
 import chess.model.ConfigurationView
 
 trait MoveRanker {
+
+  /** Convenience values to use when ranking moves into two groups. */
+  val RANKING_HIGH = 1;
+  val RANKING_LOW = 0;
+
   /**
    * @param A non-null list of legal moves.
    * @return A possibly empty list of non empty move lists where the first move list contains the most preferred moves.
