b61c64e70d1d1f993f247cc4989d10e2b635286b
Janek Bogucki <janekdb@gmail.com>
2012-11-23 09:30:51 +0000

    Seal class

--- src/chess/model/GameChanged.scala
+++ src/chess/model/GameChanged.scala
@@ -2,7 +2,7 @@ package chess.model
 
 import GameOutcomeModes.GameOutcomeMode
 
-abstract class GameChanged
+sealed abstract class GameChanged
 
 case class Won(colour: Colour, winMode: GameOutcomeMode) extends GameChanged
 case class Drawn(drawMode: GameOutcomeMode) extends GameChanged
