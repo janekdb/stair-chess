e791d06c6d298f2fa2761df4044df64fa9c76792
Janek Bogucki <janekdb@gmail.com>
2012-11-30 20:08:10 +0000

    Add player based on CaptureEvadingRanker

--- src/chess/player/Players.scala
+++ src/chess/player/Players.scala
@@ -4,6 +4,7 @@ import chess.model.ConfigurationView
 import chess.model.MoveExplorer
 import chess.ranker.CheckMatingRanker
 import chess.ranker.CapturingRanker
+import chess.ranker.CaptureEvadingRanker
 
 object Players {
 
@@ -19,4 +20,8 @@ object Players {
     new ShellPlayer("Capturing Player", colour, explorerFactory, ranker)
   }
 
+  def capturingEvadingPlayer(colour: Colour, explorerFactory: ConfigurationView => MoveExplorer): Player = {
+    val ranker = new CaptureEvadingRanker(explorerFactory, colour)
+    new ShellPlayer("Capture Evading Player", colour, explorerFactory, ranker)
+  }
 }
