3b0aeb08203de9bbf33a4966b56cc63b7782c2c1
Janek Bogucki <janekdb@gmail.com>
2012-12-04 20:49:46 +0000

    Allow listener configuration change to be achieved via listener substitution.

---
+++ src/chess/ui/NoUI.scala
@@ -0,0 +1,17 @@
+package chess.ui
+import chess.model.GameChangedSubscriber
+import chess.model.BoardChangedSubscriber
+import chess.model.GameChanged
+import chess.model.BoardChanged
+import chess.model.PiecePlaced
+
+/**
+ * A UI event listener that does nothing.
+ */
+object NoUI extends BoardChangedSubscriber with GameChangedSubscriber {
+
+  def onGameChanged(event: GameChanged) = Unit
+  def onBoardChanged(events: List[BoardChanged]) = Unit
+  def onPiecePlaced(event: PiecePlaced) = Unit
+
+}
