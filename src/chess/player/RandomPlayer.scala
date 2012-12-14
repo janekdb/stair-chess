d8112a8357981e6b4a6eb8115e6d13dfc0bf5d9b
Janek Bogucki <janekdb@gmail.com>
2012-12-14 19:11:00 +0000

    Remove TODO

--- src/chess/player/RandomPlayer.scala
+++ src/chess/player/RandomPlayer.scala
@@ -4,8 +4,6 @@ import chess.model.Move
 import chess.model.{ Configuration, MoveExplorer }
 import chess.model.Colour
 
-// TODO: Replace RandomPlayer with RandomRanker in combination with ShellPlayer
-
 /**
  * A player that selects moves at random.
  */
