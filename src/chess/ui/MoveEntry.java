e884901aae3856a4796c9f367f6241560c6f85f0
Janek Bogucki <janekdb@gmail.com>
2013-01-04 23:01:08 +0000

    Define API for UI components that allow move selection

---
+++ src/chess/ui/MoveEntry.java
@@ -0,0 +1,22 @@
+package chess.ui;
+
+import java.awt.Container;
+
+/**
+ * The API to a component allowing a user to select a move.
+ */
+public interface MoveEntry {
+
+	void enable();
+
+	void disable();
+
+	Container getContainer();
+
+	/**
+	 * @param listener
+	 *            The object that will receive entered move notifications
+	 */
+	void addMoveEntryListener(MoveEntryListener listener);
+
+}
