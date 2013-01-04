31a6ca42f871035f022264f7e075332659d0fc4f
Janek Bogucki <janekdb@gmail.com>
2013-01-04 23:03:32 +0000

    Implement MoveEntry with a JTextField

---
+++ src/chess/ui/InputFieldMoveEntry.java
@@ -0,0 +1,47 @@
+package chess.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class InputFieldMoveEntry implements MoveEntry {
+
+	private final JPanel contentPane = new JPanel(new BorderLayout());
+
+	private final JTextField inputField = new JTextField(4);
+
+	public InputFieldMoveEntry() {
+		contentPane.add(inputField, BorderLayout.NORTH);
+	}
+
+	@Override
+	public void enable() {
+		throw new RuntimeException("TODO");
+	}
+
+	@Override
+	public void disable() {
+		throw new RuntimeException("TODO");
+	}
+
+	@Override
+	public Container getContainer() {
+		return contentPane;
+	}
+
+	@Override
+	public void addMoveEntryListener(final MoveEntryListener listener) {
+		ActionListener a = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				listener.onMoveEntry(e);
+			}
+		};
+		inputField.addActionListener(a);
+	}
+
+}
