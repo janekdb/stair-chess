e272371a453d48056f5ac4f539f40298d2caa835
Janek Bogucki <janekdb@gmail.com>
2013-01-09 19:27:05 +0000

    Add complementary test

--- src/chess/player/BlockingPlayerTest.scala
+++ src/chess/player/BlockingPlayerTest.scala
@@ -11,33 +11,11 @@ import chess.model.MovePiece
 object BlockingPlayerTest extends Test with TestUtils with Main {
 
   def runTests {
-    getBlocksUntilUseInvoked
-    useBlockUntilGetInvoked
+    getBlocksUntilSetInvoked
+    setBlocksUntilGetInvoked
   }
 
-  //  private def actorTest {
-  //
-  //    class Ping extends 
-  //      def act() {
-  //        .SECONDS.sleep(4)
-  //        .println("Hi")
-  //      }
-  //    }
-  //
-  //    class Pong extends 
-  //      def act() {
-  //        .println("Pong")
-  //      }
-  //    }
-  //    val ping = new Ping
-  //    ping.start
-  //    val pong = new Pong
-  //    pong.start
-  //    .SECONDS.sleep(10)
-  //
-  //  }
-
-  private def getBlocksUntilUseInvoked {
+  private def getBlocksUntilSetInvoked {
     val p = new BlockingPlayer("Test")
     val conf = getConf
     val cd1 = new CountDownLatch(1)
@@ -50,19 +28,39 @@ object BlockingPlayerTest extends Test with TestUtils with Main {
         cd2.countDown
       }
     }
-    val getter = new Getter
-    getter.start
+    (new Getter).start
     cd1.await
     assert(moveOpt.isEmpty)
-    p.useMove("e1e2")
+    p.setMove("e1e2")
     cd2.await
     assertEquals(Some(new MovePiece("e1e2")), moveOpt)
   }
 
-  private def useBlockUntilGetInvoked = fail
+  private def setBlocksUntilGetInvoked {
+    val p = new BlockingPlayer("Test")
+    val conf = getConf
+    val cd1 = new CountDownLatch(1)
+    val cd2 = new CountDownLatch(1)
+    var set = false
+    class Setter extends Actor {
+      def act() {
+        cd1.countDown
+        p.setMove("e1e2")
+        set = true
+        cd2.countDown
+      }
+    }
+    (new Setter).start
+    cd1.await
+    assertFalse(set)
+    val Some(move) = p.getMove(conf)
+    cd2.await
+    assertEquals(new MovePiece("e1e2"), move)
+  }
 
   private def getConf = {
     val conf = new GridConfiguration
+    addKings(conf)
     conf
   }
 }
