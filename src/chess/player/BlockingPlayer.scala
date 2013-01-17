2fe201bf83277243573b070689b234ff6cb684b9
Janek Bogucki <janekdb@gmail.com>
2013-01-17 09:18:01 +0000

    Convert from Move to MoveFactory

--- src/chess/player/BlockingPlayer.scala
+++ src/chess/player/BlockingPlayer.scala
@@ -1,23 +1,26 @@
 package chess.player
 import java.util.concurrent.SynchronousQueue
-
 import chess.model.Configuration
 import chess.model.Move
+import chess.model.Colour
+import chess.model.MoveFactory
 
 /**
  * This class can be used as a blocking proxy to some source of move selections.
  */
-class BlockingPlayer(val name: String) extends Player {
+class BlockingPlayer(val colour: Colour, val name: String) extends Player {
 
-  private val q = new SynchronousQueue[Option[Move]]
+  private val q = new SynchronousQueue[MoveFactory]
 
-  def getMove(configuration: Configuration): Option[Move] = q take
+  def getMove(configuration: Configuration): Option[Move] = (q take) getMove (colour, configuration)
 
   def getName: String = name
+
+  def getColour = colour
 
   /**
    * @param Will return immediately if getMove has been called and is still blocked
    * otherwise will block until getMove is called.
    */
-  def setMove(move: Move): Unit = q put Some(move)
+  def setMoveFactory(moveFactory: MoveFactory): Unit = q put moveFactory
 }
