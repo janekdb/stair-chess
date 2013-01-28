6d67bf854b429ab1acd44b875e442bf8c25b7af8
Janek Bogucki <janekdb@gmail.com>
2013-01-28 22:24:58 +0000

    Add TODO

--- src/chess/model/Piece.scala
+++ src/chess/model/Piece.scala
@@ -4,6 +4,7 @@ sealed abstract class Piece {
   def movements: List[(Int, Int)] = List()
   def movements(colour: Colour): List[(Int, Int)] = movements
 }
+// TODO: Convert pieces to case objects
 case class Pawn extends Piece {
   override def movements(colour: Colour): List[(Int, Int)] = {
     val s = colour.pawnRowIncrement
@@ -14,4 +15,4 @@ case class Rook extends Piece { override def movements = List((1, 0), (0, -1), (
 case class Knight extends Piece { override def movements = List((1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-1, 2), (-2, 1)) }
 case class Bishop extends Piece { override def movements = List((1, 1), (1, -1), (-1, -1), (-1, 1)) }
 case class King extends Piece { override def movements = List((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)) }
-case class Queen extends Piece { override def movements = List((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)) } 
+case class Queen extends Piece { override def movements = List((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)) }
