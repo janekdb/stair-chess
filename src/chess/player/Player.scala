c1ceb0fddc35c6427b5c4f9a6eb8e8c49c7b2195
Janek Bogucki <janekdb@gmail.com>
2013-02-01 19:06:01 +0000

    Add TODO

--- src/chess/player/Player.scala
+++ src/chess/player/Player.scala
@@ -4,6 +4,7 @@ import chess.model.Move
 import chess.model.Configuration
 
 trait Player {
+  // TODO: Consider using Either to return Resign or Option[Move]
   def getMove(configuration: Configuration): Option[Move]
   def getName: String
 }
