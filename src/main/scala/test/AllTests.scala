09d60eec4d03f033d7242861797a6062220d67bf
Janek Bogucki <janekdb@gmail.com>
2021-04-10 23:50:44 +0100

    Drop reflective access of structural type member

--- src/main/scala/test/AllTests.scala
+++ src/main/scala/test/AllTests.scala
@@ -5,8 +5,9 @@ object AllTests {
   def tests: List[Test] = {
     var tests: List[Test] = Nil
     // TODO: Determine if each test suite could be added via classpath scanning
-    //   which would allow all tests to be looped over to run them.
-    implicit def addTest(any: Test) = new { def ++ : Unit = { tests ::= any } }
+    // which would allow all tests to be looped over to run them.
+
+    implicit class TestOps(any: Test) { def ++ : Unit = { tests ::= any } }
 
     import chess.model._
     PositionTest.++
